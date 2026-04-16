f20491b69ced22894b3d2dc953585cc2bb91c076
Tontun <pisutyimkuson@gmail.com>
2026-04-16 13:18:56 +0700

    Fix Vercel 404: Remove --webpack from build, add vercel.json, simplify config

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Config options
-  webpack: (config) => {
-    // Explicitly handle webpack if needed since the user used --webpack flag
-    return config;
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
-
-/*
-import withPWAInit from "next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
-export default withPWA(nextConfig);
-*/
